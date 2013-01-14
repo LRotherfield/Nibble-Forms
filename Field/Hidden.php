86d9b433549917a75f0caa0cffbe5290ba3b652e
Luke Rotherfield <luke@oxygenthinking.com>
2013-01-14 13:44:44 +0000

    hidden forms label false]

--- Field/Hidden.php
+++ Field/Hidden.php
@@ -12,6 +12,7 @@ class Hidden extends Text
     public function returnField($form_name, $name, $value = '') 
     {
         $this->field_type = 'hidden';
+        $this->label = false;
         return parent::returnField($form_name, $name, $value);
     }
 

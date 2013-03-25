a990001eddff2b53903c4097d892357404560d2e
Luke Rotherfield <luke@oxygenthinking.com>
2013-03-25 16:59:01 +0000

    Only loop through data if data is an array

--- Nibble/NibbleForms/Field/MultipleOptions.php
+++ Nibble/NibbleForms/Field/MultipleOptions.php
@@ -21,13 +21,13 @@ abstract class MultipleOptions extends BaseOptions
             if ($this->minimum_selected && count($val) < $this->minimum_selected) {
                 $this->error[] = sprintf('at least %s options must be selected', $this->minimum_selected);
             }
+            foreach ($val as $answer) {
+                if (in_array($answer, $this->false_values)) {
+                    $this->error[] = "$answer is not a valid choice";
+                }
+            }
         } elseif ($this->required) {
             $this->error[] = 'is required';
-        }
-        foreach ($val as $answer) {
-            if (in_array($answer, $this->false_values)) {
-                $this->error[] = "$answer is not a valid choice";
-            }
         }
 
         return !empty($this->error) ? false : true;
